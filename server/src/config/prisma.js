e6b4060c2b97032110f93461fb854e5da984dff0
akira4n <yobiijo94@gmail.com>
2026-09-22 23:25:58 +0700

    chore(prisma): init prisma

---
+++ server/src/config/prisma.js
@@ -0,0 +1,16 @@
+const { PrismaClient } = require('@prisma/client');
+const { PrismaMariaDb } = require('@prisma/adapter-mariadb');
+
+const adapter = new PrismaMariaDb({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+module.exports = prisma;
